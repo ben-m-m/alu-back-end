c1729b533b62e666538ca058cda57ade3bc6655c
ben-m-m <mainaben23@gmail.com>
2026-06-23 22:31:41 +0200

    Fix documentation formatting and add newline at end of file in employee data script

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """Get employee data.
-First line: Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+First line: 
+Employee EMPLOYEE_NAME is done with tasks(DONE_TASKS/TOTAL_TASKS):
 EMPLOYEE_NAME: name of the employee
 NUMBER_OF_DONE_TASKS: number of completed tasks
-TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of completed and non-completed tasks"""
+TOTAL_NUMBER_OF_TASKS: 
+total number of tasks, which is the sum of completed and non-completed tasks
+"""
 import requests
 import sys
 
@@ -36,3 +39,4 @@ if __name__ == "__main__":
     # Print completed task titles
     for task in completed:
         print("\t {}".format(task.get("title")))
+        
